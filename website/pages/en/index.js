0151c1a0ec4368270dc54c47424752bb91f8c2cf
Patricia Perozo <pperozo@pinterest.com>
2021-05-10 23:03:11 -0700

    Adding custom CSS

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -63,7 +63,7 @@ class HomeSplash extends React.Component {
         <div className="inner">
           <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
           <PromoSection>
-            <img src={siteConfig.profilePhoto} alt="My Profile Photo" border-radius="50%" max-width="60%" />
+            <img src={siteConfig.profilePhoto} alt="My Profile Photo" id="profile-img" />
           </PromoSection>
         </div>
       </SplashContainer>
